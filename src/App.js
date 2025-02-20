7d77ae570cca741223b54d8410c9071b5047cd78
chintandevka2108 <chintangadhavi511@gmail.com>
2025-02-20 22:03:14 +0530

    Usestate & useeffect fetch api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Day-1/Home';
+import DayNight from './Day-4/DayNight';
+import Text from './Day-2/Text';
+import Api from './Day-3/Api';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <>
+    {/* <Home></Home> */}
+    {/* <DayNight></DayNight> */}
+    {/* <Text></Text> */}
+    <Api></Api>
+    </>
   );
 }
 
